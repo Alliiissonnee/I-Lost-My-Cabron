247116a7801c2b5ab7ceff70da320baa765d969f
wall966 <wallace.pliss@hotmail.con>
2026-06-26 10:06:48 +0200

    Nouvelle fichier FormTrouve

--- caramelo-app/app.js
+++ caramelo-app/app.js
@@ -1,6 +1,8 @@
 /**Installation do (dotenv) le lien est securise avec gitIGNORE dans le fichier (.env)  doit être tout en haut du fichier*/
 require('dotenv').config();
 
+
+
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
@@ -16,7 +18,9 @@ var User = require('./models/usersJSON');
 var petJSON = require('./models/petJSON');
 var annoncesRouter = require('./routes/annonces');
 var app = express();
+ 
 
+app.use(cors({ origin: "http://localhost:5173" }));
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -26,12 +30,13 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/annonces', annoncesRouter);
-app.use('/petJSON', petJSON);
+app.use('/', petJSON);
 
 /*Connection du Back-end avec la base de donnes(MongoDB) (verifier le fichier(.env)*/
 const mongoURI = process.env.MONGO_URI;
 mongoose.connect(mongoURI)
   .then(() => console.log('MongoDB compass connecté'))
   .catch(err => console.error('Erreur connexion:', err));
+
 
 module.exports = app;
